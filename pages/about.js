2571aff732b181c153682fd880b674224072333b
Robin Millette <robin@millette.info>
2017-04-04 21:50:39 -0400

    Simplify.

--- pages/about.js
+++ pages/about.js
@@ -2,70 +2,28 @@ import React from 'react'
 import PouchDBChanges from 'react-pouchdb-changes'
 import Layout from '../components/layout'
 
-export default class MyLuckNo extends React.Component {
+export default class CouchdbChanges extends React.Component {
   constructor (...args) {
     super(...args)
-    // this.state = { randomNo: null, doc: null, cnt: 0 }
-    this.state = { doc: null, cnt: 0 }
+    this.state = { doc: null }
   }
-
-/*
-  componentDidMount () {
-    this.recalculate()
-  }
-
-  recalculate () {
-    this.setState({
-      randomNo: Math.ceil(Math.random() * 100)
-    })
-  }
-*/
 
   render () {
-    // const { randomNo, doc, cnt } = this.state
-    const { doc, cnt } = this.state
-
-/*
-    if (randomNo === null) {
-      return (<p>Please wait..</p>)
-    }
-    // This is an experimental JavaScript feature we can get
-    // using babel-preset-stage-0
-    const message = do {
-      if (randomNo < 30) {
-        // eslint-disable-next-line no-unused-expressions
-        'Do not give up. Try again.'
-      } else if (randomNo < 60) {
-        // eslint-disable-next-line no-unused-expressions
-        'You are a lucky guy'
-      } else {
-        // eslint-disable-next-line no-unused-expressions
-        'You are soooo lucky!'
-      }
-    }
-*/
-
     return (
       <PouchDBChanges
         dbUrl='http://localhost:5993/nd1'
         changesOpts={{
-          // since: 'now',
           live: true,
           include_docs: true
         }}
-        onChange={(change) => {
-          this.setState({doc: change.doc, cnt: cnt + 1})
-        }}
-        onError={(err) => console.log(err)}
+        onChange={(change) => this.setState({ doc: change.doc })}
       >
-
         <Layout title='About us' pathname='/about'>
-
           <div>
-            <p>Count! {cnt}</p>
-            <pre>{JSON.stringify(doc, null, '  ')}</pre>
+            <p>
+              id: {this.state.doc && this.state.doc._id} and rev: {this.state.doc && this.state.doc._rev}
+            </p>
           </div>
-
         </Layout>
       </PouchDBChanges>
     )
